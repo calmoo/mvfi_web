ef59286f8f6790315d4c83ab1685cd60e685803d
Noah <noah@karat.io>
2020-09-06 21:26:42 +0100

    Added error messaging and preliminary file upload lists.

--- app.py
+++ app.py
@@ -31,31 +31,45 @@ def allowed_file(filename):
     else:
         return False
 
+
+def process_files(uploaded_files):
+
+    fileset = {"audio_files": [],
+               "image_file": "file.jpg"}
+
+    for file in uploaded_files:
+
+        file_is_allowed = allowed_file(file.filename)
+        if file and file_is_allowed:
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            if file_is_allowed is "audio":
+                fileset["audio_files"].append(file_path)
+            elif file_is_allowed is "image":
+                fileset["image_file"] = file_path
+
+    return fileset
+
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-
     if request.method == 'POST':
         uploaded_files = request.files.getlist("file[]")
-        fileset = {"audio_files": [],
-                   "image_file": "file.jpg"}
+        processed_files = process_files(uploaded_files)
+        if processed_files:
+            return render_template('index.html', selected=uploaded_files)
 
-        for file in uploaded_files:
+        encoded_video_files = (encoder.encode_video_new(processed_files))
 
-            file_is_allowed = allowed_file(file.filename)
-            if file and file_is_allowed:
-                filename = secure_filename(file.filename)
-                file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-                file.save(file_path)
-                if file_is_allowed is "audio":
-                    fileset["audio_files"].append(file_path)
-                elif file_is_allowed is "image":
-                    fileset["image_file"] = file_path
+        if encoded_video_files:
+            return render_template('uploads.html', filenames=encoded_video_files)
+        else:
+            return render_template('index.html', error="No Files selected")
 
-        encoded_video_files = (encoder.encode_video_new(fileset))
-        # return redirect(url_for('uploaded_file', filename=encoded_video_file[0]))
-        return render_template('uploads.html', filenames=encoded_video_files)
 
     return render_template('index.html', )
+
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
